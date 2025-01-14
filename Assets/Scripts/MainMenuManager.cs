9241c8b0d46418069091162afe98d81e08afdad6
Nikhil J <nikhiljammal@gmail.com>
2025-01-14 12:20:03 -0500

    aids

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,8 +3,43 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject controlPanel;
+    public GameObject settingPanel;
     public void Play()
     {
         SceneManager.LoadScene(1);
     }
+    public void ShowControls()
+    {
+
+        controlPanel.SetActive(true); // Show the panel
+
+    }
+
+    // Method to close the controls text box
+    public void CloseControls()
+    {
+
+        controlPanel.SetActive(false); // Hide the panel
+
+    }
+    public void toggleControls()
+    {
+        if (controlPanel.activeSelf) CloseControls();
+        else ShowControls();
+    }
+    public void ShowSettings()
+    {
+        settingPanel.SetActive(true);
+    }
+    public void CloseSettings() 
+    {
+        settingPanel.SetActive(false);
+    }
+    public void toggleSettings()
+    {
+        if (settingPanel.activeSelf) CloseSettings();
+        else ShowSettings();
+    }
 }
+
